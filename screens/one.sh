234e494541681e42bf22de033fa2e384773d00e1
GyanPrakashRaj <thecornerstandingman@gmail.com>
2022-03-22 22:28:10 +0530

    lesspunch

---
+++ screens/one.sh
@@ -0,0 +1,26 @@
+# screen sharing system.
+
+# Unfortunately, this doesn't work very well, because Xorg prevents the
+# mouse pointer from going to that off screen area.
+
+add_virtual_screen_below() {
+  # get fb size WxH
+  W=4000
+  H=2640
+
+  xrandr --fb ${W}x$((${H}+1080))
+  xrandr --setmonitor VIRTUAL 1920/332x1080/187+0+$H none
+
+  ffmpeg -video_size 1920x1080 -framerate 30 \
+    -f x11grab -i :0.0+0,$H xv
+}
+
+add_virtual_screen_to_the_right() {
+  W=7040
+  H=1440
+
+  xrandr --fb $((${W}+1920))x${H}
+  xrandr --setmonitor VIRTUAL 1920/332x1080/187+${W}+0 none
+  ffmpeg -video_size 1920x1080 -framerate 30 \
+    -f x11grab -i :0.0+$W,0 xv
+}
